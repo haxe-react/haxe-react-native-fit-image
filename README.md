5832ecc81f79c0fb4d75be8c90d763417240a49a
Kevin Leung <kevinresol@gmail.com>
2017-01-05 11:53:31 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# haxe-react-native-fit-image
+
+Haxe Extern for react-native-fit-image
---
+++ src/react/native/fit_image/FitImage.hx
@@ -0,0 +1,4 @@
+package react.native.fit_image;
+
+@:jsRequire('react-native-fit-image', 'default')
+extern class FitImage extends react.ReactComponent {}
